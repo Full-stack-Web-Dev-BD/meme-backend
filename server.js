a73714943ec566a112eff1a53db48b741b39ddb3
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2022-12-23 21:40:03 -0800

    cors issue

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ var fs = require("fs");
 const User = require('./models/User');
 
 const app = express();
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
 app.use(morgan('dev'))
 app.use(cors())
 
