88024b822dbc385e8d6a089daa1a1d7528170a39
user <user@gmail.com>
2023-01-04 12:18:51 +0600

    chs

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,8 +3,8 @@ import { toast } from "react-toastify";
 import { getUserFromToken } from "../Util";
 import socketIOClient from 'socket.io-client'
 
-export const baseURL = "http://localhost:4000"
-// export const baseURL = "http://89.116.225.104:4000"
+// export const baseURL = "http://localhost:4000"
+export const baseURL = "http://89.116.225.104:4000"
 
 export const socket = socketIOClient(baseURL, { transports: ['websocket', 'polling', 'flashsocket'] })
 // export const baseURL = "https://meme-backend-api.herokuapp.com"
