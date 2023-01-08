56201da0b9fbd45d70fbc33be4ff4d3312cfcdbe
sivakumar <svguru20@gmail.com>
2023-01-08 16:07:34 +0530

    Socket origins allow in server js file

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(cors())
 
 const server = http.createServer(app);
 const io = socketio(server);
+io.set('origins', '*:*');
 const port =process.env.PORT || 5001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
