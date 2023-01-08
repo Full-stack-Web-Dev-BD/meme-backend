d7625395ed0f5dba7f2c3e4f09049779a1afdee9
user <user@gmail.com>
2023-01-08 18:16:30 +0600

    https on  ip

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -9,7 +9,7 @@ export const baseURL= "https://memechallenge.me"
 
 
 
-export const socket = socketIOClient("http://89.116.225.104:5001", {
+export const socket = socketIOClient("https://89.116.225.104:5001", {
      transports: ['websocket', 'polling', 'flashsocket'] ,
      withCredentials: true
     }) 
